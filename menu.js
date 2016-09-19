02dba9355d0623e0ecd3d73d518aa6fdeed221b0
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2016-09-19 13:20:37 -0700

    play sound on menu click

--- menu.js
+++ menu.js
@@ -4,14 +4,14 @@
       return 53 * Math.floor(53 * Math.random()); // 53 pixels, 52 cards (0..52) inclusive
     }
     function randomCoords() {
-      var width = window.screen.width,
-          height = window.screen.height,
-          radius = Math.sqrt(Math.pow(width/2, 2), Math.pow(height, 2)) + 80,
+      var width = $(window).width(),
+          height = $(window).height(),
+          radius = Math.sqrt(Math.pow(width/2+200, 2) + Math.pow(height, 2)),
           radians = Math.PI * Math.random(),
           dirx = Math.cos(radians) * radius + width/2,
           diry = Math.sin(radians) * radius;
 
-      console.log('coords: '+dirx+','+diry);
+      // console.log('radius, coords: '+radius+', '+dirx+'&'+diry);
       return {x: dirx, y: diry};
     }
     var cnt=0;
@@ -19,12 +19,14 @@
       var $elem = $(this),
           cardBg = $elem.children('.card-bg'),
           textBg = $elem.children('.text-bg'),
-          $sound = new Audio('/sfx/cardSlide4.wav');
+          hovSound = new Audio('/sfx/cardSlide5.wav'),
+          clickSound = new Audio('/sfx/cardShove1.wav');
 
       cardBg.click(function(e) {
         $(this).css('position', 'absolute');
         var link = $(this).children('a')[0],
             coords = randomCoords();
+        clickSound.play();
         $(this).animate(
           {
             'left': '' + coords.x + 'px',
@@ -45,7 +47,7 @@
           var bgposx = "-" + (cardx) + "px";
           randomCoords();
           cardBg.css({'background-position-x': bgposx});
-          cardBg.animate({'top': '0px'}, 200, 'swing', function(){$sound.play();});
+          cardBg.animate({'top': '0px'}, 200, 'swing', function(){hovSound.play();});
           textBg.animate({'top': '0px'}, 200);
         },
         function() {
